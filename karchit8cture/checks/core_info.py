971b4d0a3152acc05bbbdaaf67ce2a6cfd29e519
Shon Paz <shonpaz123@gmail.com>
2023-11-09 20:09:57 +0200

    Finished testing minimum node count and resources

--- karchit8cture/checks/core_info.py
+++ karchit8cture/checks/core_info.py
@@ -5,8 +5,7 @@ def check_node_readiness(v1):
 
     try:
         # Make sure to gather all nodes' status 
-        nodes = v1.list_node()
-        for node in nodes.items:
+        for node in v1.items:
             if node.status.conditions[3].status != "True":
                 return False
 
@@ -15,16 +14,49 @@ def check_node_readiness(v1):
     
     return True
 
-def check_node_crs(v1):
+def check_control_plane_count(v1):
+
+    minimum_control_plane_nodes = 3 # In an HA based cluster, minimum number is 3 
+
+    try:
+        # Return flase if control plane node count is less than 3
+        if len(v1.items) < minimum_control_plane_nodes:
+            return False
+
+    except Exception as e:
+        print(f"Error while checking control plane nodes count: {str(e)}")
+    
+    return True
+
+def check_data_plane_count(v1):
+
+    minimum_data_plane_nodes = 2 # In an HA based cluster, minimum number is 3 
+
+    try:
+        # Return false if data plane node count is less than 2
+        if len(v1.items) < minimum_data_plane_nodes:
+            return False
+
+    except Exception as e:
+        print(f"Error while checking data plane nodes count: {str(e)}")
+    
+    return True
+def verify_hardware_capacity(v1):
+
+    min_memory = 16 
+    min_cpu = 4
 
     try:
         # Make sure to gather all nodes' status 
-        nodes = v1.list_node()
-        for node in nodes.items:
-            if node.status.conditions[3].status != "True":
-                return False
+        for node in v1.items:
+           memory = int(node.status.capacity['memory'].rstrip("Ki")) / (1000**2) 
+           cpu = int(node.status.capacity['cpu'])
+
+           # Return false if node capacity is less than expected 
+           if memory < min_memory or cpu < min_cpu:
+            return False
 
     except Exception as e:
-        print(f"Error while checking node readiness: {str(e)}")
+        print(f"Error while checking node capacity resources: {str(e)}")
     
     return True
--- karchit8cture/karchit8cture.py
+++ karchit8cture/karchit8cture.py
@@ -7,11 +7,19 @@ def validate_k8s_cluster():
         # Load the Kubernetes configuration from your kubeconfig file
         config.load_kube_config()
 
-        # Create a Kubernetes API client
-        v1 = client.CoreV1Api()
+        # Create a Kubernetes API client for nodes information gathering 
+        nodes_v1 = client.CoreV1Api().list_node()
 
+        # Collect results by executing a different check each time 
         results = {
-            "Node Readiness": core_info.check_node_readiness(v1),
+            "Node Readiness": core_info.check_node_readiness(nodes_v1),
+            "Minimum Control Plane Nodes": 
+                core_info.check_control_plane_count(client.CoreV1Api().
+                                                    list_node(label_selector='node-role.kubernetes.io/control-plane')),
+            "Minimum Data Plane Nodes": 
+                core_info.check_data_plane_count(client.CoreV1Api().
+                                                    list_node(label_selector='node-role.kubernetes.io/worker')),
+            "Node Capacity": core_info.verify_hardware_capacity(nodes_v1),
         }
         return results
 
